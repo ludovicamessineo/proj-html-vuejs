8e130470f6b3a470dbde79f482dd72325b7937f4
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-05-30 17:42:23 +0200

    setting header and first banner

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header d-flex justify-content-between align-items-center">
+    <div class="header-logo">
+        <img :src="require('../assets/theme_eduprime_logo.png')" alt="">
+    </div>
+    <div class="header-menu">
+        <ul class="d-flex align-items-center">
+            <li>Home</li>
+            <li>Courses</li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    width: 100%;
+    height: 80px;
+    padding: 2rem;
+    background-color: rgba(229, 103, 104, .1);
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 10;
+
+    img {
+        width: 10rem;
+    }
+
+    ul {
+        margin: 0;
+    }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+      <section class="first-banner">
+
+      </section>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .first-banner {
+        height: 500px;
+        background-image: url(../assets/theme_slider2_bg-1.jpg);
+        background-position: center;
+        background-size: cover;
+        position: relative;
+    }
+
+    .first-banner::after {
+        content: '';
+        background-color: rgba(229, 103, 104, .9);
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
