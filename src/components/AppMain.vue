cff75edda977dd0db61a70e46c78d42c358f296e
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-06-01 11:24:26 +0200

    add dinamic cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -37,16 +37,41 @@
               <div class="uni-graphic">
                   <img class="icon" :src="require('../assets/Clock-and-Bell.png')" alt="">  
                   <h2>University Year</h2>  
-                  <img :src="require('../assets/Timeline-Item.png')" alt="">                
+                  <div class="timeline">
+                      <div class="timeline-img">
+                        <img :src="require('../assets/Timeline-Item.png')" alt="">                     
+                      </div>
+                      <div class="timeline-text">
+                          
+                      </div>
+                  </div>
               </div>
 
               <div class="uni-events">
                   <img class="icon" :src="require('../assets/upcoming-events-calendar-icon.png')" alt="">
                   <h2>Upcoming Events</h2>
                   <div class="row">
-                      <div class="col-4">
-                          <h4>Coaching Sessions</h4>
+                      <div class="col card" v-for="(item, index) in coachingCard" :key="index">
+                          <h4> {{item.title}} </h4>
+                          <small> {{item.date}} </small>
+                          <p> {{item.text}} </p>
+                          <button class="ms_btn-red">Find More</button>
                       </div>
+                  </div>
+                  <button class="ms_btn mt-4">View all Events</button>
+              </div>
+          </div>
+      </section>
+
+      <section>
+          <div class="ms_container ms_mt">
+              <h3>Latest courses</h3>
+              <div class="row">
+                  <div class="col course-card" v-for="(item, index) in courseCard" :key="index">
+                      <img :src="require(`../assets/${item.img}`)" alt="">
+                      <h5> {{item.text}} </h5>
+                      <p> {{item.teacher}} </p>
+                      <p> {{item.price}} </p>
                   </div>
               </div>
           </div>
@@ -94,11 +119,62 @@ export default {
                }
            ]
        } 
+    },
+    data() {
+        return {
+            coachingCard: [
+                {
+                    title: "Coaching Session",
+                    date: "20 May 21.30 PM",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor[...]"                   
+                },
+                {
+                    title: "Coaching Session",
+                    date: "24 Mar 18.00 PM",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor[...]"  
+                }, 
+                {
+                    title: "Coaching Session",
+                    date: "12 Feb 13.30 PM",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor[...]"  
+                }
+            ]
+        }
+    },
+    data() {
+        return {
+            courseCard: [
+                {
+                    img: "Decisions-icon.png",
+                    text: "Make Better Decisions",
+                    teacher: "Teacher: James Collins",
+                    price: "Price: $21.00"
+                },
+                {
+                    img: "Speaker-icon.png",
+                    text: "How to be a speaker",
+                    teacher: "Teacher: James Collins",
+                    price: "Price: Free"
+                },
+                {
+                    img: "Network-icon.png",
+                    text: "Network Introductions",
+                    teacher: "Teacher: James Collins",
+                    price: "Price: Free"
+                },
+                {
+                    img: "Brand-icon.png",
+                    text: "Brand Management",
+                    teacher: "Teacher: James Collins",
+                    price: "Price: Free"
+                }
+            ]
+        }
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
     /* SECTION 1 */
     .first-banner {
         height: 500px;
@@ -138,9 +214,43 @@ export default {
     }
     /* /SECTION 3 */
 
-
+    // SECTION 5
     .events {
         background-color: #e56768;
         text-align: center;
+        color: white;
+    }
+
+    .card {
+        background-color: white;
+        color: black;
+        margin: 2rem .5rem;
+        padding: .8rem .4rem;
+        border-radius: 10px;
+        font-size: .7rem;
+        position: relative;
+
+        button {
+            padding: .4rem .6rem;
+            width: 5rem;
+            position: absolute;
+            left: 35%;
+            bottom: -15px;
+            box-shadow: 1px 1px 5px black;
+        }
+    }
+    // SECTION 5
+
+    // SECTION 6
+    .course-card {
+       border: 1px solid lightgray; 
+       text-align: center;
+       font-size: .6rem;
+       margin: 1rem;
+       padding: .8rem;
+
+       img {
+           width: 3rem;
+       }
     }
 </style>
